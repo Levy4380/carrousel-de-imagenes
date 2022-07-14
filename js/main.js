52ad3f51cb4b4dbf818e0f0a20bbce8c69697a19
Levy4380 <tomaslevy1999@gmail.com>
2022-07-14 14:04:10 -0300

    pull

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+let cambioColor = (color) => {
+    let i = 0;
+    for (i;i<20;i++)
+    setInterval(
+    $('.color').css({
+        'background-color' : color 
+    }),1000)
+}
+
+// let lectorColor = () => {
+//     if 
+// }
+// setInterval(cambioColor('blue'),1005);//1 seg
+// setInterval(cambioColor('yellow'),1000);
+
+
